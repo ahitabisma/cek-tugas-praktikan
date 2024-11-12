47c1d909be8e133a1221027fc3b8e8cfa6929a51
Ahita Bisma Adlula <ahita.adlula@mhs.unsoed.ac.id>
2024-11-12 18:53:34 +0700

    test slack

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -105,7 +105,6 @@ const formatDate = (timestamp: string) => {
             <th class="text-center">Timestamp</th>
           </tr>
         </thead>
-        <!-- Data -->
         <tbody>
           <tr v-for="(item, index) in responseStore.responses" :key="index">
             <td class="text-center">{{ item['Pertemuan ke'] }}</td>
